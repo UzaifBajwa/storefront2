3d18c76bb6ed63dddfa96be07af58642a5717c45
Uzaif Bajwa <uzaifbajwa11@gmail.com>
2022-08-09 18:45:28 +0500

    Initializing Seralizers for product API.

---
+++ store/serializers.py
@@ -0,0 +1,9 @@
+from http.client import ImproperConnectionState
+from unittest.util import _MAX_LENGTH
+from rest_framework import serializers
+
+
+class ProductSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
