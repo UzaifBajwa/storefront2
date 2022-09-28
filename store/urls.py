a03f2bdb557fee9aa5a223ec40ce3b68a3b03954
Uzaif Bajwa <uzaifbajwa11@gmail.com>
2022-09-29 03:14:37 +0500

    Creating Generic views for ProductList and CollectionList.

--- store/urls.py
+++ store/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
     path('products/<int:id>/', views.ProductDetails.as_view()),
+    path('collections/', views.CollectionList.as_view()),
     path('collections/<int:pk>/', views.collection_detail),
-    path('collections/', views.collection_list),
+    # path('collections/', views.collection_list),
     path('products/', views.show_products),
     path('customer/', views.show_customers),
     path('order/', views.show_orders)
--- store/views.py
+++ store/views.py
@@ -1,7 +1,9 @@
+import imp
 from itertools import product
 from os import stat
 import re
 from turtle import title
+from typing import List
 from urllib import request
 from winreg import QueryInfoKey
 from . import views
@@ -14,6 +16,7 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
@@ -21,21 +24,38 @@ from .serializers import ProductSerializer, CollectionSerializer
 from store import serializers
 
 
-# Creating API views
+# Creating Generic API views
 
-class ProductList(APIView):
-    def get(self, request):
-        queryset = Product.objects.select_related('collection').all()
-        serializer = ProductSerializer(
-            queryset, many=True, context={'request': request})
+
+class CollectionList(ListCreateAPIView):
+    queryset = Collection.objects.annotate(
+        products_count=Count('products')).all()
+    serializer_class = CollectionSerializer
+
+
+""" @api_view(['GET', 'POST'])
+def collection_list(request):
+    if request.method == 'GET':
+        queryset = Collection.objects.annotate(
+            products_count=Count('product')).all()
+        serializer = CollectionSerializer(queryset, many=True)
         return Response(serializer.data)
-
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
+    elif request.method == 'POST':
+        serializer = CollectionSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        # serializer.validated_data
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED) """
+
+
+class ProductList(ListCreateAPIView):
+    def get_queryset(self):
+        return Product.objects.select_related('collection').all()
+
+    def get_serializer_class(self):
+        return ProductSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
 
 
 class ProductDetails(APIView):
@@ -79,20 +99,6 @@ def collection_detail(request, pk):
             return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(['GET', 'POST'])
-def collection_list(request):
-    if request.method == 'GET':
-        queryset = Collection.objects.annotate(
-            products_count=Count('product')).all()
-        serializer = CollectionSerializer(queryset, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = CollectionSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 # Create your views here.
